6db9a21d618fa71b395cb7f105634d249023cdc4
Agnaldo Junior <agnaldo88@gmail.com>
2024-11-20 20:39:25 -0300

    Estados e Navegação - Navegação com expo-router

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import {View, Image, TouchableOpacity, FlatList, Modal, Text} from "react-native"
 import {MaterialIcons} from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from  "./styles"
 import { colors } from "@/styles/colors"
@@ -17,7 +18,7 @@ export default function Index(){
 
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
                 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
@@ -43,7 +44,7 @@ export default function Index(){
             />
 
                 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         
