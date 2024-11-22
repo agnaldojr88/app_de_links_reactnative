8c20fd989839ba58e4d10a18ea34975ab5c288be
Agnaldo Junior <agnaldo88@gmail.com>
2024-11-21 21:16:30 -0300

    Implementado o UseEffect para exibir a lista do AsyncStorage

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,19 +1,34 @@
-import {View, Image, TouchableOpacity, FlatList, Modal, Text} from "react-native"
+import {View, Image, TouchableOpacity, FlatList, Modal, Text, Alert} from "react-native"
 import {MaterialIcons} from "@expo/vector-icons"
 import { router } from "expo-router"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import { styles } from  "./styles"
 import { colors } from "@/styles/colors"
 import { categories } from "@/utils/categories"
+import { linkStorage, LinkStorage } from "@/storage/link-storage"
 
 import { Link } from "@/components/link"
 import { Categories } from "@/components/categories"
 import { Option } from "@/components/option"
 
 export default function Index(){
-
+    const [links, setLinks] = useState<LinkStorage[]>([])
     const [category, setCategory] = useState(categories[0].name)
+
+    async function getLinks(){
+        try{
+            const response = await linkStorage.get()
+            setLinks(response)
+            
+        }catch(error){
+            Alert.alert("Não foi possivel listar os links")
+        }
+    }
+
+    useEffect(() => {
+        getLinks()
+    }, [category])
 
     return (
         <View style={styles.container}>
@@ -32,13 +47,13 @@ export default function Index(){
             
            
             <FlatList 
-                data={["1", "2", "3"]}
-                keyExtractor={item => item}
-                renderItem={() => (
+                data={links}
+                keyExtractor={item => item.id}
+                renderItem={( {item} ) => (
 
                     <Link 
-                        name="Rocketseat"
-                        url="www.google.com.br" 
+                        name={item.name}
+                        url={item.url}
                         onDetails={() => console.log("Clicou")} 
                     /> 
                 )}
